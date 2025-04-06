57028c8ef87526b91090f04b27538c31fa63bc02
Oketch <oketchngwani@gmail.com>
2025-04-07 05:20:38 +0900

    final

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,27 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    countdown = 10
+    while countdown > 0:
+        print(countdown)
+        countdown -= 1
+    print("Happy New Year!")
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    return [i ** 2 for i in int_list]
+numbers = [1, 2, 3, 4, 5]
+squared_numbers = square_integers(numbers)
+print(squared_numbers)
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for number in range(1, 101):
+        if number % 3 == 0 and number % 5 == 0:
+            print("FizzBuzz")
+        elif number % 3 == 0:
+            print("Fizz")
+        elif number % 5 == 0:
+            print("Buzz")
+        else:
+            print(number)
+           
+    
